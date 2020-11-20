1d2394915ab11ae117a7b704b04ed1da9269b1b9
nasirmovlamov <movlamovnasir2000@gmail.com>
2020-11-20 17:13:46 +0400

    structur page is ready

---
+++ StructurPage/structur.js
@@ -0,0 +1,9 @@
+let countOfImageCard = document.getElementsByClassName('card').length;
+        console.log(document.getElementById("container").lastElementChild);
+        if (((countOfImageCard - 1 ) % 3) ===0) {
+            document.getElementById('container').lastElementChild.className += " lastItemCenterChecker";
+        }
+        else
+        {
+
+        }
